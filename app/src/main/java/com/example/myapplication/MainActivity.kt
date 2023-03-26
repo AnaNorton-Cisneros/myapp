18567a91b31e633ceef169435cecc467dffc829d
AnaNorton-Cisneros <ananortoncisneros@gmail.com>
2023-03-26 15:28:44 -0500

    i added a start button and a text box. You got it!

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import androidx.compose.material.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
+//components I want:
+//buttons, card, text button
+
+
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
@@ -22,13 +29,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
+                Text(text = "Welcome to a coffee tracker. Track how much coffee you drink throughout the day to make sure you're consuming a healthy amount." , color = Color.Blue )
+
+
+                Button(onClick = { "Type in how much coffee you have consumed so far:" }) {
+                    Text("START")
+                }}
             }
-            //adding change to commit
+
         }
+
     }
-}
+
 
 @Composable
 fun Greeting(name: String) {
@@ -38,6 +52,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+    Text(text = "Hello")
     MyApplicationTheme {
         Greeting("Android")
     }
