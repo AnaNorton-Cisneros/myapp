d848926a2a22f249bd8f7062212abc675fb4b64a
ananortoncisneros <ananortoncisneros@gmail.com>
2023-04-02 15:25:31 -0500

    i added a bunch of stuff

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import androidx.compose.material.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 //components I want:
@@ -20,9 +22,11 @@ import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -31,17 +35,99 @@ class MainActivity : ComponentActivity() {
                 ) {
 
                 }
-                Text(text = "Welcome to a coffee tracker. Track how much coffee you drink throughout the day to make sure you're consuming a healthy amount." , color = Color.Blue )
+                Column {
+                    Text(text = "Find out which foods are best to combat sickness!")
 
+                    Text(
+                        text = "Select your symptoms:",
+                        color = Color.Red
+                    )
+                    val checkedState = remember { mutableStateOf(true) }
+                    Row {
+                        Checkbox(
+                            checked = checkedState.value,
+                            onCheckedChange = { checkedState.value = it }
+                        )
+                        Text("dizziness", modifier = Modifier.width(150.dp))
+                    }
+                    Row {
 
-                Button(onClick = { "Type in how much coffee you have consumed so far:" }) {
-                    Text("START")
-                }}
+                        val checkedState2 = remember { mutableStateOf(true) }
+                        Checkbox(
+                            checked = checkedState2.value,
+                            onCheckedChange = { checkedState2.value = it }
+                        )
+                        Text("heartburn")
+                    }
+                    Row {
+                        val checkedState3 = remember { mutableStateOf(true) }
+                        Checkbox(
+                            checked = checkedState3.value,
+                            onCheckedChange = { checkedState3.value = it }
+                        )
+                        Text("headache")
+                    }
+                    Row {
+
+                        val checkedState4 = remember { mutableStateOf(true) }
+                        Checkbox(
+                            checked = checkedState4.value,
+                            onCheckedChange = { checkedState4.value = it }
+                        )
+                        Text("nausea")
+                    }
+
+                    val checkedState5 = remember { mutableStateOf(true) }
+                    Row {
+                        Checkbox(
+                            checked = checkedState5.value,
+                            onCheckedChange = { checkedState5.value = it }
+                        )
+                        Text("cough/cold")
+                    }
+                    Row {
+
+                        val checkedState6 = remember { mutableStateOf(true) }
+                        Checkbox(
+                            checked = checkedState6.value,
+                            onCheckedChange = { checkedState6.value = it }
+                        )
+                        Text("indigestion")
+                    }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Button(onClick = {
+                            //enter here ↓
+
+                            //enter here ^^^
+                        }) {
+                            Text("GO")
+                        }
+//public class MyActivity extends Activity {
+//      protected void onCreate(Bundle icicle) {
+//          super.onCreate(icicle);
+//
+//          setContentView(R.layout.content_layout_id);
+//
+//          final CheckBox checkBox = (CheckBox) findViewById(R.id.checkbox_id);
+//          if (checkBox.isChecked()) {
+//              checkBox.setChecked(false);
+//          }
+//      }
+//  }
+                    }
+
+                }
             }
 
         }
-
     }
+}
+
 
 
 @Composable
